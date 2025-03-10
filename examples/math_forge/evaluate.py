be2fee0db6db44d9c2003d195d078dcdb9792856
Xabier <xabierj@gmail.com>
2025-03-10 12:40:40 +0100

    replaced azure- with openai model endpoint id

--- examples/math_forge/evaluate.py
+++ examples/math_forge/evaluate.py
@@ -9,7 +9,6 @@ import json
 from datetime import UTC, datetime
 from pathlib import Path
 
-from dotenv import load_dotenv
 from loguru import logger
 
 from ebiose.compute_intensive_batch_processor.compute_intensive_batch_processor import (
@@ -18,50 +17,76 @@ from ebiose.compute_intensive_batch_processor.compute_intensive_batch_processor 
 from ebiose.core.agent_factory import AgentFactory
 from examples.math_forge.math_forge import MathLangGraphForge
 
-load_dotenv()
+
+def main(
+    train_csv_path: str,
+    test_csv_path: str,
+    agent_json_file: str,
+    n_problems: int,
+    budget: float,
+    model_endpoint_id: str,
+) -> None:
+    
+    # instantiating the forge
+    forge = MathLangGraphForge(
+        train_csv_path=train_csv_path,
+        test_csv_path=test_csv_path,
+        n_problems=n_problems,
+    )
+
+    # loading agent
+    with Path.open(agent_json_file) as json_file:
+        agent_configuration = json.load(json_file)
+
+    agent = AgentFactory.load_agent(
+        agent_config=agent_configuration,
+        model_endpoint_id=model_endpoint_id,
+        input_model=forge.agent_input_model,
+        output_model=forge.agent_output_model,
+    )
+
+    # generating the compute token
+    ComputeIntensiveBatchProcessor.initialize()
+    master_token_id = ComputeIntensiveBatchProcessor.acquire_master_token(budget)
+    compute_token_id = ComputeIntensiveBatchProcessor.generate_token(budget, master_token_id)
+
+    # running evaluation on test set
+    t0 = datetime.now(UTC)
+    fitness = asyncio.run(
+        forge.compute_fitness(
+            agent=agent,
+            compute_token_id=compute_token_id,
+            mode="test",
+        ),
+    )
+
+    # getting cost
+    cost = ComputeIntensiveBatchProcessor.get_token_cost(compute_token_id)
+
+    logger.info(f"Evaluation of agent {agent.id} on test set took {datetime.now(UTC) - t0}")
+    logger.info(f"Computed fitness is: {fitness}, for cost: {cost} $")
 
 
-TRAIN_CSV_PATH = "./examples/math_forge/gsm8k_train.csv" # the train dataset
-TEST_CSV_PATH = "./examples/math_forge/gsm8k_test.csv" # the test dataset
-AGENT_JSON_FILE = Path("data/2025-02-28_17-49-05/generation=2/agents/agent-211c7fe5-d329-470e-bdd9-ae7ee6ce0be3.json")
-N_PROBLEMS = 2 # number of problems to evaluate on
-BUDGET = 0.1 # budget for evaluation in dollars
-MODEL_ENDPOINT_ID = "gpt-4o-mini" # model endpoint id
-# instantiating the forge
-forge = MathLangGraphForge(
-    train_csv_path=TRAIN_CSV_PATH,
-    test_csv_path=TEST_CSV_PATH,
-    n_problems=N_PROBLEMS,
-)
+if __name__ == "__main__":
 
-# loading agent
-with Path.open(AGENT_JSON_FILE) as json_file:
-    agent_configuration = json.load(json_file)
+    # loading dotenv
+    from dotenv import load_dotenv
+    load_dotenv()
 
-agent = AgentFactory.load_agent(
-    agent_config=agent_configuration,
-    model_endpoint_id=MODEL_ENDPOINT_ID,
-    input_model=forge.agent_input_model,
-    output_model=forge.agent_output_model,
-)
+    # evaluation parameters
+    AGENT_JSON_FILE = "data/2025-03-10_11-29-05/generation=4/agents/agent-ba8a9d8f-36bc-44b7-aaaf-be10c821f995.json"
+    TRAIN_CSV_PATH = "./examples/math_forge/gsm8k_train.csv" # the train dataset
+    TEST_CSV_PATH = "./examples/math_forge/gsm8k_test.csv" # the test dataset
+    N_PROBLEMS = 2 # number of problems to evaluate on
+    BUDGET = 0.05 # budget for evaluation in dollars
+    MODEL_ENDPOINT_ID = "gpt-4o-mini" # model endpoint id
 
-# generating the compute token
-ComputeIntensiveBatchProcessor.initialize()
-master_token_id = ComputeIntensiveBatchProcessor.acquire_master_token(BUDGET)
-compute_token_id = ComputeIntensiveBatchProcessor.generate_token(BUDGET, master_token_id)
-
-# running evaluation on test set
-t0 = datetime.now(UTC)
-fitness = asyncio.run(
-    forge.compute_fitness(
-        agent=agent,
-        compute_token_id=compute_token_id,
-        mode="test",
-    ),
-)
-
-# getting cost
-cost = ComputeIntensiveBatchProcessor.get_token_cost(compute_token_id)
-
-logger.info(f"Evaluation of agent {agent.id} on test set took {datetime.now(UTC) - t0}")
-logger.info(f"Computed fitness is: {fitness}, for cost: {cost} $")
+    # running the evaluation
+    main(
+        train_csv_path=TRAIN_CSV_PATH,
+        test_csv_path=TEST_CSV_PATH,
+        agent_json_file=AGENT_JSON_FILE,
+        n_problems=N_PROBLEMS,
+        budget=BUDGET,
+        model_endpoint_id=MODEL_ENDPOINT_ID,
+    )
--- examples/math_forge/run.py
+++ examples/math_forge/run.py
@@ -15,40 +15,68 @@ from loguru import logger
 from ebiose.core.evo_forging_cycle import EvoForgingCylceConfig
 from examples.math_forge.math_forge import MathLangGraphForge
 
-load_dotenv()
-
-# the path where results will be saved
-timestamp = datetime.now(tz=UTC).strftime("%Y-%m-%d_%H-%M-%S")
-SAVE_PATH = Path(f"./data/{timestamp}/")
-if not SAVE_PATH.exists():
-    SAVE_PATH.mkdir(parents=True)
-
 logger.remove()
 logger.add(sys.stderr, level="DEBUG")
-logger.add(SAVE_PATH / "all_logs.log", rotation="10 MB", level="DEBUG")
+
+def main(
+        train_csv_path: str,
+        test_csv_path: str,
+        n_problems: int,
+        budget: float,
+        save_path: Path,
+    ) -> None:
+    forge = MathLangGraphForge(
+        train_csv_path=train_csv_path,
+        test_csv_path=test_csv_path,
+        n_problems=n_problems,
+    )
+
+    cycle_config = EvoForgingCylceConfig(
+        budget=budget,
+        n_agents_in_population=2,
+        n_selected_agents_from_ecosystem=0,
+        n_best_agents_to_return=2,
+        replacement_ratio=0.5,
+        save_path=save_path,
+    )
+
+    best_agents, best_finess = asyncio.run(
+        forge.run_new_cycle(config=cycle_config)
+    )
+
+    logger.info("Best agents and their fitness:")
+    forge.display_results(best_agents, best_finess)
+
+if __name__ == "__main__":
+
+    # loading dotenv
+    from dotenv import load_dotenv
+    load_dotenv()
 
 
-N_PROBLEMS = 2 # number of problems to evaluate on, per generation
-TRAIN_CSV_PATH = "./examples/math_forge/gsm8k_train.csv" # the train dataset
-TEST_CSV_PATH = "./examples/math_forge/gsm8k_test.csv" # the test dataset
+    # the path where results will be saved
+    timestamp = datetime.now(tz=UTC).strftime("%Y-%m-%d_%H-%M-%S")
+    SAVE_PATH = Path(f"./data/{timestamp}/")
+    if not SAVE_PATH.exists():
+        SAVE_PATH.mkdir(parents=True)
 
-forge = MathLangGraphForge(
-    train_csv_path=TRAIN_CSV_PATH,
-    test_csv_path=TEST_CSV_PATH,
-    n_problems=N_PROBLEMS,
-)
+    # logging config
+    logger.remove()
+    logger.add(sys.stderr, level="DEBUG")
+    logger.add(SAVE_PATH / "all_logs.log", rotation="10 MB", level="DEBUG")
 
-cycle_config = EvoForgingCylceConfig(
-    budget=0.05,
-    n_agents_in_population=2,
-    n_selected_agents_from_ecosystem=0,
-    n_best_agents_to_return=2,
-    replacement_ratio=0.5,
-    save_path=SAVE_PATH,
-)
 
-best_agents, best_finess = asyncio.run(forge.run_new_cycle(config=cycle_config))
+    # run parameters
+    BUDGET = 0.05 # budget in dollars
+    N_PROBLEMS = 2 # number of problems to evaluate on, per generation
+    TRAIN_CSV_PATH = "./examples/math_forge/gsm8k_train.csv" # the train dataset
+    TEST_CSV_PATH = "./examples/math_forge/gsm8k_test.csv" # the test dataset
 
-logger.info("Best agents and their fitness:")
-forge.display_results(best_agents, best_finess)
-
+    # running the forge cycle
+    main(
+        train_csv_path=TRAIN_CSV_PATH,
+        test_csv_path=TEST_CSV_PATH,
+        n_problems=N_PROBLEMS,
+        budget=BUDGET,
+        save_path=SAVE_PATH,
+    )
